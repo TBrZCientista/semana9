2eed48e5d7ad7c9c0054ada66c8b6743b2ae07f2
Felipe <felipefreitasdaschagas@hotmail.com>
2022-03-30 06:35:03 -0300

    Subindo imagens e trabalhando com possíveis erros de extensão de arquivos

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,10 +1,25 @@
 const fs = require('fs')
 const { formatWithOptions } = require('util')
+const path = require('path')
 
 module.exports = (caminho, nomeDoArquivo, callbackImagemCriada)=> {
-    const novoCaminho = `./assets/imagens/${nomeDoArquivo}`
+    
+    const tiposValidos = ['jpg','png', 'jpeg']
+    const tipo = path.extname(caminho)
+    const tipoEhValido = tiposValidos.indexOf(tipo.substring(1))!== -1
+    
+    if(tipoEhValido){
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
 
-    fs.createReadStream(caminho)
+        fs.createReadStream(caminho)
         .pipe(fs.createWriteStream(novoCaminho))
-        .on('finish',()=> callbackImagemCriada(novoCaminho))
+        .on('finish',()=> callbackImagemCriada(false, novoCaminho))
+    } else {
+        const erro = 'Tipo de arquivo inválido'
+        console.log('Erro!! Extensão de arquivo errada')
+        callbackImagemCriada(erro)
     }
+    
+ }
+
+ 
--- models/pets.js
+++ models/pets.js
@@ -6,18 +6,22 @@ class Pet {
     adiciona(pet,res){
         const query = 'INSERT INTO agenda_petshop.pets SET?'
         
-        uploadDeArquivo(pet.imagem, pet.nome, (novoCaminho) => {
-            const novoPet = {nome: pet.nome, imagem: novoCaminho}
+        uploadDeArquivo(pet.imagem, pet.nome, (erro, novoCaminho) => {
+
+            if(erro){
+                res.status(400).json({erro})
+            } else {
+                const novoPet = {nome: pet.nome, imagem: novoCaminho}
             
-            conexao.query(query, novoPet, erro => {
+                conexao.query(query, novoPet, erro => {
                 if(erro){
                     res.status(400).json(erro)
                 }else {
                     res.status(200).json(novoPet)
                 }
             })
-        })
-        
+            }
+        }) 
     }
 }
 
